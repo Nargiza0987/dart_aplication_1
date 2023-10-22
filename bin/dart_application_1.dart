1d31ccbedcd71e8e31172f592957c58f8cd71008
Nargiza0987 <kulm1987@gmail.com>
2023-10-22 15:17:11 +0300

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,9 @@
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main(List<String> arguments) {
+  print('Minar menin balam!');
+  print('I am flutter developer');
+  print('I am beautiful');
+  print('I am rich woman');
+}
